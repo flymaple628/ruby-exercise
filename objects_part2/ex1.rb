722861e96e097d312e823230f282d0aed0ee4002
Aaron Chen <flymaple628@gmail.com>
2015-06-09 18:11:01 +0800

    finish part2 ex2

--- objects_part2/ex1.rb
+++ objects_part2/ex1.rb
@@ -1,8 +1,40 @@
 class MyCar
+
+		def initialize(year,model,color)
+		@year=year
+		@model=model
+		@color=color
+		@speed=0
+	end
+	attr_reader:year
+	attr_accessor:model
+	attr_accessor:color
+
+	def speedUp(num)
+		@speed+=num
+		puts "Your speed is #{@speed}"
+	end
+
+	def speednow
+		puts "Your speed is #{@speed}"
+	end
+
+	def brake(num)
+		@speed-=num
+		puts "Your speed is #{@speed}"
+	end
+	def shutoff()
+		@speed=0
+		puts "Your are shut off."
+	end
 	def self.gasMileage(gas,mile)
 		puts "#{mile/gas} miles per gallon of gas"
 	end
-
+	def to_s
+		puts "my #{@color} color is #{@model} since #{@year}"
+	end
 end
 
 MyCar.gasMileage(12,100)
+mycar=MyCar.new("1995","RX8","red")
+mycar.to_s()
