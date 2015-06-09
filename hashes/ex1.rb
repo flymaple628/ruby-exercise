0ddf2780e5d3d38a19b6109f219fced6a41a1236
Aaron Chen <flymaple628@gmail.com>
2015-06-09 16:29:36 +0800

    finish hashes 1

---
+++ hashes/ex1.rb
@@ -0,0 +1,7 @@
+family = {  uncles: ["bob", "joe", "steve"],
+            sisters: ["jane", "jill", "beth"],
+            brothers: ["frank","rob","david"],
+            aunts: ["mary","sally","susan"]
+          }
+array= family.select{ |e| e == :sisters || e==:brothers }
+puts array
